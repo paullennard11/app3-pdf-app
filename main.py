6c9fdb34b99f80a5e1f096efcff4c1ee3201643f
Lennard <lennardpolito@gmail.com>
2023-08-30 18:10:19 +0800

    Day 24: Add more pages for each topic

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ for index, row in df.iterrows():
     pdf.add_page()
 
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(0, 0, 254)
+    pdf.set_text_color(100, 1000, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
          ln=1)
     pdf.line(10, 21, 200, 21)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
